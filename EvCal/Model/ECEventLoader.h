1e80d514b7de34f33a6ed4e74ac525ae5e3fb411
Thomas Leu <tomleu@gmail.com>
2015-05-17 17:02:15 +0800

    Add implementation for event loading, make event loader a singleton

--- EvCal/Model/ECEventLoader.h
+++ EvCal/Model/ECEventLoader.h
@@ -8,6 +8,56 @@
 
 #import <Foundation/Foundation.h>
 
+#define ECEventLoaderAuthorizationStatusChangedNotification @"AuthorizationStatusChanged"
+
+typedef NS_ENUM(NSUInteger, ECAuthorizationStatus) {
+    ECAuthorizationStatusNotDetermined,
+    ECAuthorizationStatusDenied,
+    ECAuthorizationStatusAuthorized,
+};
+
 @interface ECEventLoader : NSObject
+
+
+@property (nonatomic, readonly) ECAuthorizationStatus authorizationStatus;
+
+//------------------------------------------------------------------------------
+// @name Accessing shared instance
+//------------------------------------------------------------------------------
+
+/**
+ *  Returns the shared instance of ECEventLoader
+ */
++ (instancetype)sharedInstance;
+
+//------------------------------------------------------------------------------
+// @name Loading User Events
+//------------------------------------------------------------------------------
+
+/**
+ *  Creates and returns an array of all of the user's events which fall within 
+ *  a given date range.
+ *
+ *  @param startDate The start date of the range of user events to load.
+ *  @param endDate   The end date of the range of user events to load.
+ *
+ *  @return An array (possibly empty) of EKEvents or nil if user has denied 
+ *          calendar access.
+ */
+- (NSArray*)loadEventsFrom:(NSDate*)startDate to:(NSDate*)endDate;
+
+/**
+ *  Creates and returns an array of the user's events in the given calendar which
+ *  fall within the given date range.
+ *
+ *  @param startDate The start date of the range of user events to load.
+ *  @param endDate   The end date of the range of user events to load.
+ *  @param calendars The calendars from which to load events. Passing nil
+ *                   indicates that events from all calendars should be loaded.
+ *
+ *  @return An array (possibly empty) of EKEvents or nil if user had denied 
+ *          calendar access.
+ */
+- (NSArray*)loadEventsFrom:(NSDate*)startDate to:(NSDate*)endDate in:(NSArray*)calendars;
 
 @end
--- EvCal/Model/ECEventLoader.m
+++ EvCal/Model/ECEventLoader.m
@@ -11,33 +11,94 @@
 #import "ECEventLoader.h"
 @interface ECEventLoader()
 
-@property (nonatomic) EKEventStore* eventStore;
+@property (nonatomic, readwrite) ECAuthorizationStatus authorizationStatus;
+
+@property (nonatomic, strong) EKEventStore* eventStore;
 
 @end
 
 @implementation ECEventLoader
 
+#pragma mark - Lifecycle and Properties
+
++ (instancetype)sharedInstance
+{
+    __strong static ECEventLoader* _loader = nil;
+    static dispatch_once_t once;
+    dispatch_once(&once, ^{
+        _loader = [[ECEventLoader alloc] init];
+    });
+    
+    return _loader;
+}
+
+
 - (EKEventStore*)eventStore
 {
     if (!_eventStore) {
         _eventStore = [[EKEventStore alloc] init];
-        
-        [self requestAccessToEvents];
     }
     
     return _eventStore;
 }
 
+- (ECAuthorizationStatus)authorizationStatus
+{
+    switch ([EKEventStore authorizationStatusForEntityType:EKEntityTypeEvent]) {
+        case EKAuthorizationStatusNotDetermined:
+            return ECAuthorizationStatusNotDetermined;
+            
+        // treat restricted and denied as the same
+        case EKAuthorizationStatusDenied:
+        case EKAuthorizationStatusRestricted:
+            return ECAuthorizationStatusDenied;
+            
+        case EKAuthorizationStatusAuthorized:
+            return ECAuthorizationStatusAuthorized;
+    } ;
+}
 
-#pragma mark - Accessing User Events
+
+#pragma mark - Loading User Events
+
+- (NSArray*)loadEventsFrom:(NSDate *)startDate to:(NSDate *)endDate
+{
+    return [self loadEventsFrom:startDate to:endDate in:nil];
+}
+
+- (NSArray*)loadEventsFrom:(NSDate *)startDate to:(NSDate *)endDate in:(NSArray *)calendars
+{
+    switch (self.authorizationStatus) {
+        case ECAuthorizationStatusNotDetermined:
+            [self requestAccessToEvents];
+            return nil;
+            
+        case ECAuthorizationStatusDenied:
+            return nil;
+        
+        case ECAuthorizationStatusAuthorized: {
+            NSPredicate* dateRangePredicate = [self.eventStore predicateForEventsWithStartDate:startDate endDate:endDate calendars:calendars];
+            NSArray* events = [self.eventStore eventsMatchingPredicate:dateRangePredicate];
+            
+            return events;
+        }
+    }
+}
+
+#pragma mark - Managing Calendar Access
 
 - (void)requestAccessToEvents
 {
-    [_eventStore requestAccessToEntityType:EKEntityTypeEvent completion:^(BOOL granted, NSError* err) {
-        if (granted) {
-            
+    [self.eventStore requestAccessToEntityType:EKEntityTypeEvent completion:^(BOOL granted, NSError* err) {
+        if (!err) {
+            [self postAuthorizationStatusChangedNotification];
         }
     }];
+}
+
+- (void)postAuthorizationStatusChangedNotification
+{
+    [[NSNotificationCenter defaultCenter] postNotificationName:ECEventLoaderAuthorizationStatusChangedNotification object:nil];
 }
 
 @end
