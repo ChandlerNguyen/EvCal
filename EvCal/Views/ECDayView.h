9b0d98acd285cd3190857fd8b7b45448454e23cc
Thomas Leu <tomleu@gmail.com>
2015-05-18 22:46:10 +0800

    Add interface for day view

--- EvCal/Views/ECDayView.h
+++ EvCal/Views/ECDayView.h
@@ -6,8 +6,14 @@
 //  Copyright (c) 2015 spitzgoby LLC. All rights reserved.
 //
 
+@class ECEventView;
 #import <UIKit/UIKit.h>
 
 @interface ECDayView : UIScrollView
+
+- (void)addEventView:(ECEventView*)eventView;
+- (void)addEventViews:(NSArray*)eventViews;
+- (void)removeEventView:(ECEventView*)eventView;
+- (void)removeEventViews:(NSArray*)eventViews;
 
 @end
