780768b42be0f5c43309de24760d5804b4559c83
Thomas Leu <tomleu@gmail.com>
2015-05-19 19:20:30 +0800

    Remove revoke access calendar method

--- Pods/JPSimulatorHacks/JPSimulatorHacks/JPSimulatorHacks.h
+++ Pods/JPSimulatorHacks/JPSimulatorHacks/JPSimulatorHacks.h
@@ -43,7 +43,6 @@
 + (BOOL)grantAccessToPhotosForBundleIdentifier:(NSString *)bundleIdentifier;
 
 + (BOOL)grantAccessToCalendar;
-+ (BOOL)revokeAccessToCalendar;
 + (BOOL)grantAccessToCalendarForBundleIdentifier:(NSString *)bundleIdentifier;
 
 + (void)setTimeout:(NSTimeInterval)timeout;
