2b8408fe3a0f2e67ff96e0fe8f7467abf8e0dc7a
Thomas Leu <tomleu@gmail.com>
2015-06-02 15:10:04 +0800

    Add edit event controller to project

---
+++ EvCal/Controllers/ECEditEventViewController.h
@@ -0,0 +1,13 @@
+//
+//  ECEditEventViewController.h
+//  EvCal
+//
+//  Created by Tom on 6/2/15.
+//  Copyright (c) 2015 spitzgoby LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ECEditEventViewController : UIViewController
+
+@end
---
+++ EvCal/Controllers/ECEditEventViewController.m
@@ -0,0 +1,13 @@
+//
+//  ECEditEventViewController.m
+//  EvCal
+//
+//  Created by Tom on 6/2/15.
+//  Copyright (c) 2015 spitzgoby LLC. All rights reserved.
+//
+
+#import "ECEditEventViewController.h"
+
+@implementation ECEditEventViewController
+
+@end
