50eb135954e6f66a4ee53653b25213fc095664df
Thomas Leu <tomleu@gmail.com>
2015-06-12 10:20:59 +0800

    Update data source tests and add layout tests

--- EvCalTests/ECDayViewEventsLayoutTests.m
+++ EvCalTests/ECDayViewEventsLayoutTests.m
@@ -31,6 +31,7 @@
 
 @property (nonatomic) BOOL eventViewsRequested;
 @property (nonatomic) BOOL eventViewBoundsRequested;
+@property (nonatomic) BOOL numberOfHoursRequested;
 @property (nonatomic) BOOL minimumHeightRequested;
 
 @end
@@ -56,6 +57,7 @@
     // Data source method call booleans
     self.eventViewsRequested = NO;
     self.eventViewBoundsRequested = NO;
+    self.numberOfHoursRequested = NO;
     self.minimumHeightRequested = NO;
     
     // Save events to this calendar for easier testing/removal
@@ -84,6 +86,7 @@
 
     self.eventViewsRequested = NO;
     self.eventViewBoundsRequested = NO;
+    self.numberOfHoursRequested = NO;
     self.minimumHeightRequested = NO;
 
     [self.eventStore removeCalendar:self.testCalendar commit:YES error:nil];
@@ -108,70 +111,6 @@
     return [[ECEventView alloc] initWithEvent:event];
 }
 
-// The following snippets create the displayed event views and demonstrates the
-// appropriate configuration and provides the proper frames with the default
-// test bounds.
-//
-//    NSCalendar* calendar = [NSCalendar currentCalendar];
-//    
-//    *** Standalone event view ***
-//    12:00 AM *+----------------------+
-//              | A                    |
-//     1:00 AM *+----------------------+
-//
-//    A Frame {{0, 0}, {120, 100}}
-//    
-//    NSDate* midnight = [self.testStartDate beginningOfDay];
-//    NSDate* oneAM = [calendar dateByAddingUnit:NSCalendarUnitHour value:1 toDate:midnight options:0];
-//    
-//    self.eventViews = @[[self createEventViewWithStartDate:midnight endDate:oneAM allDay:NO]];
-//
-//
-//    *** Side by side events ***
-//     2:00 AM *+----------++----------+
-//              | A        || B        |
-//     3:00 AM *+----------++----------+
-//    NSDate* twoAM = [calendar dateByAddingUnit:NSCalendarUnitHour value:2 toDate:midnight options:0];
-//    NSDate* threeAM = [calendar dateByAddingUnit:NSCalendarUnitHour value:3 toDate:midnight options:0];
-//
-//    self.eventViews = @[[self createEventViewWithStartDate:twoAM endDate:threeAM allDay:NO],
-//                        [self createEventViewWithStartDate:twoAM endDate:threeAM allDay:NO]];
-//
-//    A Frame {{200, 0}, {60, 100}}
-//    B Frame {{200, 60}, {60, 100}}
-//
-//    *** Event view that overlaps with two non-overlapping event views ***
-//     4:00 AM *+----------++----------+
-//              | A        || B        |
-//     5:00 AM  |          ++----------+
-//              |          || C        |
-//     6:00 AM  +----------++----------+
-//    NSDate* fourAM = [calendar dateByAddingUnit:NSCalendarUnitHour value:4 toDate:midnight options:0];
-//    NSDate* fiveAM = [calendar dateByAddingUnit:NSCalendarUnitHour value:5 toDate:midnight options:0];
-//    NSDate* sixAM = [calendar dateByAddingUnit:NSCalendarUnitHour value:6 toDate:midnight options:0];
-//
-//    self.eventViews = @[[self createEventViewStartDate:fourAM endDate:sixAM allDay:NO],
-//                        [self createEventViewStartDate:fourAM endDate:fiveAM allDay:NO],
-//                        [self createEventViewStartDate:fiveAM endDate:sixAM allDay:NO]];
-//
-//    A Frame {{400, 0}, {60, 200}}
-//    B Frame {{400, 60}, {60, 100}}
-//    C Frame {{500, 60}, {60, 100}}
-//
-//    *** Event view that overlaps with multiple event views that alternate overlap
-//     7:00 AM *+----------++----------+
-//              | A        || B        |
-//     8:00 AM *|          |+----++----+
-//              |          ||C   ||D   |
-//     9:00 AM *+----------++----++----+
-//
-//     A Frame {{700, 0}, {60, 200}}
-//     B Frame {{700, 60}, {60, 100}}
-//     C Frame {{800, 60}, {30, 100}}
-//     D Frame {{800, 90}, {30, 100}}
-//
-
-
 
 #pragma mark - ECDayViewEventsLayout Data Source
 
@@ -185,6 +124,12 @@
 {
     self.eventViewsRequested = YES;
     return self.eventViews;
+}
+
+- (NSInteger)numberOfHoursInDayForLayout:(ECDayViewEventsLayout *)layout
+{
+    self.numberOfHoursRequested = YES;
+    return [self.testStartDate hoursOfDay].count;
 }
 
 - (CGFloat)minimumEventHeightForLayout:(ECDayViewEventsLayout *)layout
@@ -224,6 +169,22 @@
     XCTAssertTrue(self.eventViewsRequested);
 }
 
+- (void)testEventViewLayoutAsksDataSourceForNumberOfHours
+{
+    ECEventView* eventView = [self createEventViewWithStartDate:self.testStartDate endDate:[self.testStartDate endOfHour] allDay:NO];
+    
+    [self.layout frameForEventView:eventView];
+    XCTAssertTrue(self.numberOfHoursRequested);
+}
+
+- (void)testEventViewLayoutAsksDataSourceForMinimumHeight
+{
+    ECEventView* eventView = [self createEventViewWithStartDate:self.testStartDate endDate:[self.testStartDate endOfHour] allDay:NO];
+    
+    [self.layout frameForEventView:eventView];
+    XCTAssert(self.minimumHeightRequested);
+}
+
 - (void)testEventViewLayoutRequestsBoundsAfterLayoutInvalidation
 {
     ECEventView* eventView = [self createEventViewWithStartDate:self.testStartDate endDate:[self.testStartDate endOfHour] allDay:NO];
@@ -238,14 +199,26 @@
 
 - (void)testEventViewLayoutRequestsEventViewsAfterLayoutInvalidation
 {
-    ECEventView* eventView = [self createEventViewWithStartDate:self.testStartDate endDate:[self.testStartDate endOfHour] allDay:NO];
+    self.eventViews = @[[self createEventViewWithStartDate:self.testStartDate endDate:[self.testStartDate endOfHour] allDay:NO]];
     
-    [self.layout frameForEventView:eventView];
+    [self.layout frameForEventView:self.eventViews.firstObject];
     [self.layout invalidateLayout];
     self.eventViewsRequested = NO;
-    [self.layout frameForEventView:eventView];
+    [self.layout frameForEventView:self.eventViews.firstObject];
     
     XCTAssertTrue(self.eventViewsRequested);
+}
+
+- (void)testEventViewLayoutRequestsNumberOfHoursAfterLayoutInvalidation
+{
+    self.eventViews = @[[self createEventViewWithStartDate:self.testStartDate endDate:[self.testStartDate endOfHour] allDay:NO]];
+    
+    [self.layout frameForEventView:self.eventViews.firstObject];
+    [self.layout invalidateLayout];
+    self.numberOfHoursRequested = NO;
+    [self.layout frameForEventView:self.eventViews.firstObject];
+    
+    XCTAssertTrue(self.numberOfHoursRequested);
 }
 
 
@@ -257,10 +230,131 @@
 
 - (void)testEventViewLayoutReturnsZeroFrameIfBoundAreZero
 {
+    self.eventViews = @[[self createEventViewWithStartDate:self.testStartDate endDate:[self.testStartDate endOfHour] allDay:NO]];
+    
     self.testBounds = CGRectZero;
     [self.layout invalidateLayout];
     
     XCTAssertTrue(CGRectEqualToRect(CGRectZero, [self.layout frameForEventView:self.eventViews.firstObject]));
+}
+
+
+#pragma mark Testing event layouts
+//  12:00 AM *+----------------------+
+//            | A                    |
+//   1:00 AM *+----------------------+
+//
+//  A Frame {{0, 0}, {120, 100}}
+- (void)testEventViewLayoutCreatesCorrectFrameForOneEvent
+{
+    NSCalendar* calendar = [NSCalendar currentCalendar];
+    NSDate* midnight = [self.testStartDate beginningOfDay];
+    NSDate* oneAM = [calendar dateByAddingUnit:NSCalendarUnitHour value:1 toDate:midnight options:0];
+
+    self.eventViews = @[[self createEventViewWithStartDate:midnight endDate:oneAM allDay:NO]];
+    
+    CGRect eventViewFrame = [self.layout frameForEventView:self.eventViews.firstObject];
+    XCTAssertTrue(CGRectEqualToRect(CGRectMake(0, 0, 120, 100), eventViewFrame));
+}
+
+//  Side by side events ***
+//   2:00 AM *+----------++----------+
+//            | A        || B        |
+//   3:00 AM *+----------++----------+
+//
+//    A Frame {{?, 200}, {60, 100}}
+//    B Frame {{?, 200}, {60, 100}}
+- (void)testEventViewLayoutCreatesCorrectFrameForTwoOverlappingEvents
+{
+    NSCalendar* calendar = [NSCalendar currentCalendar];
+    
+    NSDate* midnight = [self.testStartDate beginningOfDay];
+    NSDate* twoAM = [calendar dateByAddingUnit:NSCalendarUnitHour value:2 toDate:midnight options:0];
+    NSDate* threeAM = [calendar dateByAddingUnit:NSCalendarUnitHour value:3 toDate:midnight options:0];
+
+    ECEventView* a = [self createEventViewWithStartDate:twoAM endDate:threeAM allDay:NO];
+    ECEventView* b = [self createEventViewWithStartDate:twoAM endDate:threeAM allDay:NO];
+    self.eventViews = @[a,b];
+    
+    CGRect aFrame = [self.layout frameForEventView:a];
+    CGRect bFrame = [self.layout frameForEventView:b];
+
+    XCTAssertTrue(aFrame.origin.y == 200 && CGSizeEqualToSize(aFrame.size, CGSizeMake(60, 100)) &&
+                  bFrame.origin.y == 200 && CGSizeEqualToSize(bFrame.size, CGSizeMake(60, 100)) &&
+                  !CGRectIntersectsRect(aFrame, bFrame));
+}
+
+//    *** Event view that overlaps with two non-overlapping event views ***
+//     4:00 AM *+----------++----------+
+//              | A        || B        |
+//     5:00 AM  |          ++----------+
+//              |          || C        |
+//     6:00 AM  +----------++----------+
+//
+//    A Frame {{?, 400}, {60, 200}}
+//    B Frame {{?, 400}, {60, 100}}
+//    C Frame {{?, 500}, {60, 100}}
+- (void)testEventViewLayoutCreatesCorrectFrameForOneEventOverlappingTwoNonOverlappingEvents
+{
+    NSCalendar* calendar = [NSCalendar currentCalendar];
+    
+    NSDate* midnight = [self.testStartDate beginningOfDay];
+    NSDate* fourAM = [calendar dateByAddingUnit:NSCalendarUnitHour value:4 toDate:midnight options:0];
+    NSDate* fiveAM = [calendar dateByAddingUnit:NSCalendarUnitHour value:5 toDate:midnight options:0];
+    NSDate* sixAM = [calendar dateByAddingUnit:NSCalendarUnitHour value:6 toDate:midnight options:0];
+
+    ECEventView* a = [self createEventViewWithStartDate:fourAM endDate:sixAM allDay:NO];
+    ECEventView* b = [self createEventViewWithStartDate:fourAM endDate:fiveAM allDay:NO];
+    ECEventView* c = [self createEventViewWithStartDate:fiveAM endDate:sixAM allDay:NO];
+    self.eventViews = @[a,b,c];
+    
+    CGRect aFrame = [self.layout frameForEventView:a];
+    CGRect bFrame = [self.layout frameForEventView:b];
+    CGRect cFrame = [self.layout frameForEventView:c];
+    
+    XCTAssertTrue(aFrame.origin.y == 400 && CGSizeEqualToSize(aFrame.size, CGSizeMake(60, 200)) &&
+                  bFrame.origin.y == 400 && CGSizeEqualToSize(bFrame.size, CGSizeMake(60, 100)) &&
+                  cFrame.origin.y == 500 && CGSizeEqualToSize(cFrame.size, CGSizeMake(60, 100)) &&
+                  !CGRectIntersectsRect(aFrame, bFrame) && !CGRectIntersectsRect(aFrame, cFrame) && !CGRectIntersectsRect(bFrame, cFrame));
+}
+
+//    *** Event view that overlaps with multiple event views that alternate overlap
+//     7:00 AM *+----------++----------+
+//              | A        || B        |
+//     8:00 AM *|          |+----++----+
+//              |          ||C   ||D   |
+//     9:00 AM *+----------++----++----+
+//
+//     A Frame {{?, 700}, {60, 200}}
+//     B Frame {{?, 700}, {60, 100}}
+//     C Frame {{?, 800}, {30, 100}}
+//     D Frame {{?, 800}, {30, 100}}
+- (void)testEventViewLayoutCreatesCorrectFramesForNestedOverlappingEvents
+{
+    NSCalendar* calendar = [NSCalendar autoupdatingCurrentCalendar];
+    
+    NSDate* midnight = [self.testStartDate beginningOfDay];
+    NSDate* sevenAM = [calendar dateByAddingUnit:NSCalendarUnitHour value:7 toDate:midnight options:0];
+    NSDate* eightAM = [calendar dateByAddingUnit:NSCalendarUnitHour value:8 toDate:midnight options:0];
+    NSDate* nineAM = [calendar dateByAddingUnit:NSCalendarUnitHour value:9 toDate:midnight options:0];
+    
+    ECEventView* a = [self createEventViewWithStartDate:sevenAM endDate:nineAM allDay:NO];
+    ECEventView* b = [self createEventViewWithStartDate:sevenAM endDate:eightAM allDay:NO];
+    ECEventView* c = [self createEventViewWithStartDate:eightAM endDate:nineAM allDay:NO];
+    ECEventView* d = [self createEventViewWithStartDate:eightAM endDate:nineAM allDay:NO];
+    self.eventViews = @[a,b,c,d];
+    
+    CGRect aFrame = [self.layout frameForEventView:a];
+    CGRect bFrame = [self.layout frameForEventView:b];
+    CGRect cFrame = [self.layout frameForEventView:c];
+    CGRect dFrame = [self.layout frameForEventView:d];
+    
+    XCTAssertTrue(aFrame.origin.y == 700 && CGSizeEqualToSize(aFrame.size, CGSizeMake(60, 200)) &&
+                  bFrame.origin.y == 700 && CGSizeEqualToSize(bFrame.size, CGSizeMake(60, 100)) &&
+                  cFrame.origin.y == 800 && CGSizeEqualToSize(cFrame.size, CGSizeMake(30, 100)) &&
+                  dFrame.origin.y == 800 && CGSizeEqualToSize(dFrame.size, CGSizeMake(30, 100)) &&
+                  !CGRectIntersectsRect(aFrame, bFrame) && !CGRectIntersectsRect(aFrame, cFrame) && !CGRectIntersectsRect(aFrame, dFrame) &&
+                  !CGRectIntersectsRect(bFrame, cFrame) && !CGRectIntersectsRect(bFrame, dFrame) && !CGRectIntersectsRect(cFrame, dFrame));
 }
 
 @end
