487630be72ee865828bfc393836ba54ade83e4ca
Thomas Leu <tomleu@gmail.com>
2015-06-14 21:03:59 +0800

    Link storyboard with calendar selector class

--- EvCal/Controllers/ECEditEventCalendarViewController.m
+++ EvCal/Controllers/ECEditEventCalendarViewController.m
@@ -6,8 +6,31 @@
 //  Copyright (c) 2015 spitzgoby LLC. All rights reserved.
 //
 
+#import "ECEventStoreProxy.h"
 #import "ECEditEventCalendarViewController.h"
 
 @implementation ECEditEventCalendarViewController
+
+#pragma mark - UITableView Delegate and Data Source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [ECEventStoreProxy sharedInstance].calendars.count;
+}
+
+- (UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:@"calendarCell"];
+    
+    EKCalendar* calendar = [ECEventStoreProxy sharedInstance].calendars[indexPath.row];
+    cell.textLabel.text = calendar.title;
+    
+    return cell;
+}
 
 @end
