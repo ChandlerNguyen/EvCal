265cccbf2d83cbee6778dc7d9f4c57b97ff13495
Thomas Leu <tomleu@gmail.com>
2015-06-23 15:44:48 +0800

    Finish tests for infinite paging date view

--- EvCalTests/ECInfiniteDatePagingViewTests.m
+++ EvCalTests/ECInfiniteDatePagingViewTests.m
@@ -113,4 +113,54 @@
     XCTAssertTrue([[NSCalendar currentCalendar] isDate:dateView.date inSameDayAsDate:self.testStartDate]);
 }
 
+- (void)testInfiniteDatePagingViewRequestsPreparingPagesAfterRefresh
+{
+    self.preparePageRequested = NO;
+    [self.infiniteDatePagingView refreshPages];
+    
+    XCTAssertTrue(self.preparePageRequested);
+}
+
+- (void)testInfiniteDatePagingViewHasCorrectDateAfterRefresh
+{
+    [self.infiniteDatePagingView refreshPages];
+    
+    XCTAssertTrue([[NSCalendar currentCalendar] isDate:self.testStartDate inSameDayAsDate:self.infiniteDatePagingView.date]);
+}
+
+- (void)testInfiniteDatePagingViewVisiblePageHasCorrectDateAfterRefresh
+{
+    [self.infiniteDatePagingView refreshPages];
+    
+    ECTestsDateView* dateView = (ECTestsDateView*)self.infiniteDatePagingView.visiblePageView;
+    XCTAssertTrue([[NSCalendar currentCalendar] isDate:self.testStartDate inSameDayAsDate:dateView.date]);
+}
+
+- (void)testInfiniteDatePagingViewCallsDelegateOnScroll
+{
+    self.dateChangedCalled = NO;
+    NSCalendar* calendar = [NSCalendar currentCalendar];
+    NSDate* nextWeekDate = [calendar dateByAddingUnit:NSCalendarUnitDay value:7 toDate:self.testStartDate options:0];
+    [self.infiniteDatePagingView scrollToDate:nextWeekDate animated:NO];
+    
+    XCTAssertTrue(self.dateChangedCalled);
+}
+
+- (void)testInfiniteDatePagingViewHasCorrectDateAfterScroll
+{
+    NSCalendar* calendar = [NSCalendar currentCalendar];
+    NSDate* nextWeekDate = [calendar dateByAddingUnit:NSCalendarUnitDay value:7 toDate:self.testStartDate options:0];
+    [self.infiniteDatePagingView scrollToDate:nextWeekDate animated:NO];
+    
+    XCTAssertTrue([calendar isDate:nextWeekDate inSameDayAsDate:self.infiniteDatePagingView.date]);
+}
+
+- (void)testInfiniteDatePagingViewUpdatesVisiblePageAfterScrollingToDate
+{
+    NSCalendar* calendar = [NSCalendar currentCalendar];
+    NSDate* nextWeekDate = [calendar dateByAddingUnit:NSCalendarUnitDay value:7 toDate:self.testStartDate options:0];
+    [self.infiniteDatePagingView scrollToDate:nextWeekDate animated:NO];
+    
+    XCTAssertTrue([calendar isDate:nextWeekDate inSameDayAsDate:self.infiniteDatePagingView.date]);
+}
 @end
