27e53426ee3eabc27ba4e98082255e9a2bdd66c3
Thomas Leu <tomleu@gmail.com>
2015-06-23 14:18:32 +0800

    Add infinite date paging view test class

---
+++ EvCalTests/ECInfiniteDatePagingViewTests.m
@@ -0,0 +1,39 @@
+//
+//  ECInfiniteDatePagingViewTests.m
+//  EvCal
+//
+//  Created by Tom on 6/23/15.
+//  Copyright (c) 2015 spitzgoby LLC. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ECInfiniteDatePagingViewTests : XCTestCase
+
+@end
+
+@implementation ECInfiniteDatePagingViewTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
