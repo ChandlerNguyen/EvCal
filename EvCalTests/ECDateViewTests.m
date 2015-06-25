e621ed7979f58c61fa25581db9073d22fa8d1563
Thomas Leu <tomleu@gmail.com>
2015-06-25 18:30:03 +0800

    Add date view tests

--- EvCalTests/ECDateViewTests.m
+++ EvCalTests/ECDateViewTests.m
@@ -6,34 +6,114 @@
 //  Copyright (c) 2015 spitzgoby LLC. All rights reserved.
 //
 
+// Frameworks
+@import EventKit;
 #import <XCTest/XCTest.h>
 
+// EvCal Classes
+#import "ECDateView.h"
+
 @interface ECDateViewTests : XCTestCase
+
+@property (nonatomic, strong) NSDate* testStartDate;
 
 @end
 
 @implementation ECDateViewTests
 
+#pragma mark - Setup & Teardown
+
 - (void)setUp {
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    self.testStartDate = [NSDate date];
 }
 
 - (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    self.testStartDate = nil;
+    
     [super tearDown];
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    XCTAssert(YES, @"Pass");
+#pragma mark - Tests
+#pragma mark Creating date views
+- (void)testDateViewCanBeCreated
+{
+    ECDateView* dateView = [[ECDateView alloc] initWithDate:self.testStartDate];
+    
+    XCTAssertNotNil(dateView);
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+- (void)testDateViewHasCorrectDate
+{
+    ECDateView* dateView = [[ECDateView alloc] initWithDate:self.testStartDate];
+    
+    XCTAssertTrue([[NSCalendar currentCalendar] isDate:dateView.date inSameDayAsDate:self.testStartDate]);
+}
+
+- (void)testDateViewIsNotSelectedByDefault
+{
+    ECDateView* dateView = [[ECDateView alloc] initWithDate:self.testStartDate];
+    
+    XCTAssertFalse(dateView.isSelectedDate);
+}
+
+- (void)testDateViewCalendarsNilByDefault
+{
+    ECDateView* dateView = [[ECDateView alloc] initWithDate:self.testStartDate];
+    
+    XCTAssertNil(dateView.calendars);
+}
+
+#pragma mark Today's date
+- (void)testDateViewIsTodaysDateReturnsNoIfNotInTodaysDate
+{
+    NSDate* twoDaysAgo = [[NSCalendar currentCalendar] dateByAddingUnit:NSCalendarUnitDay value:-2 toDate:self.testStartDate options:0];
+    ECDateView* dateView = [[ECDateView alloc] initWithDate:twoDaysAgo];
+    
+    XCTAssertFalse(dateView.isTodaysDate);
+}
+
+- (void)testDateViewIsTodaysDateReturnsCorrectAnswer
+{
+    ECDateView* dateView = [[ECDateView alloc] initWithDate:self.testStartDate];
+   
+    // This test could fail if run close to the end of the day
+    XCTAssertEqual(dateView.isTodaysDate, [[NSCalendar currentCalendar] isDateInToday:self.testStartDate]);
+}
+
+#pragma mark Selecting date
+- (void)testDateViewSelectedDateCanBeChangedToYes
+{
+    ECDateView* dateView = [[ECDateView alloc] initWithDate:self.testStartDate];
+    
+    [dateView setSelectedDate:YES animated:NO];
+    
+    XCTAssertTrue(dateView.isSelectedDate);
+}
+
+- (void)testDateViewSelectedDateCanBeChangedToNo
+{
+    ECDateView* dateView = [[ECDateView alloc] initWithDate:self.testStartDate];
+    
+    [dateView setSelectedDate:YES animated:NO];
+    [dateView setSelectedDate:NO animated:NO];
+    
+    XCTAssertFalse(dateView.isSelectedDate);
+}
+
+#pragma mark Changing calendars
+
+- (void)testDateViewCalendarsCanBeSet
+{
+    EKEventStore* eventStore = [[EKEventStore alloc] init];
+    NSArray* eventCalendars = [eventStore calendarsForEntityType:EKEntityTypeEvent];
+    
+    ECDateView* dateView = [[ECDateView alloc] initWithDate:self.testStartDate];
+    dateView.calendars = eventCalendars;
+    
+    XCTAssertEqualObjects(eventCalendars, dateView.calendars);
 }
 
 @end
