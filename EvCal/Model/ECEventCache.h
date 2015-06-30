94ead325e4f1027dcc7b918c80716063f8987708
Thomas Leu <tomleu@gmail.com>
2015-06-30 18:44:56 +0800

    Add event cache and itnerface to project

---
+++ EvCal/Model/ECEventCache.h
@@ -0,0 +1,51 @@
+//
+//  ECEventCache.h
+//  EvCal
+//
+//  Created by Tom on 6/30/15.
+//  Copyright (c) 2015 spitzgoby LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class ECEventCache;
+@protocol ECEventCacheDataSource <NSObject>
+
+/**
+ *  Requests all events from the user's permanent event storage within the given
+ *  date range.
+ *
+ *  @param startDate The beginning of the date range to search for events within.
+ *  @param endDate   The end of the date range to search for events within.
+ *
+ *  @return An array of EKEvents or nil if no events fall within the range.
+ */
+- (NSArray*)storedEventsFrom:(NSDate*)startDate to:(NSDate*)endDate;
+
+@end
+
+@interface ECEventCache : NSObject
+
+// The data source is responsible for providing events from the permamant store.
+@property (nonatomic, weak) id<ECEventCacheDataSource> cacheDataSource;
+
+/**
+ *  Returns an array of events within the given date range in the given
+ *  calendars.
+ *
+ *  @param startDate The start date of the date range to search within
+ *  @param endDate   The end date of the date range to search within
+ *  @param calendars The calendars whose events should be included
+ *
+ *  @return The newly created array of events or nil if no events could be 
+ *          found. This method will always return nil if the receiver's data 
+ *          source is not set.
+ */
+- (NSArray*)eventsFrom:(NSDate*)startDate to:(NSDate*)endDate in:(NSArray*)calendars;
+
+/**
+ *  Clears the cache's stored events.
+ */
+- (void)flush;
+
+@end
---
+++ EvCal/Model/ECEventCache.m
@@ -0,0 +1,13 @@
+//
+//  ECEventCache.m
+//  EvCal
+//
+//  Created by Tom on 6/30/15.
+//  Copyright (c) 2015 spitzgoby LLC. All rights reserved.
+//
+
+#import "ECEventCache.h"
+
+@implementation ECEventCache
+
+@end
