6b52dff7a1fce930fc6cf954e863682c670f2bb7
Thomas Leu <tomleu@gmail.com>
2015-07-06 12:39:34 +0800

    Add recurrence rule tests to project

---
+++ EvCalTests/ECRecurrenceRuleFormatterTests.m
@@ -0,0 +1,39 @@
+//
+//  ECRecurrenceRuleFormatterTests.m
+//  EvCal
+//
+//  Created by Tom on 7/6/15.
+//  Copyright (c) 2015 spitzgoby LLC. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ECRecurrenceRuleFormatterTests : XCTestCase
+
+@end
+
+@implementation ECRecurrenceRuleFormatterTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
