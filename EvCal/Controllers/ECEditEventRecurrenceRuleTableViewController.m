efcf9393dcd9ebafc9c3df2c82beae3a0d936fc9
Thomas Leu <tomleu@gmail.com>
2015-07-08 16:19:31 +0800

    Add recurrence cells to view controller

--- EvCal/Controllers/ECEditEventRecurrenceRuleTableViewController.m
+++ EvCal/Controllers/ECEditEventRecurrenceRuleTableViewController.m
@@ -24,23 +24,118 @@
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 0;
+    switch (section) {
+        case kSpecificRecurrenceRuleSection:
+            return kSpecificRuleTypeCount;
+            
+        case kCustomRecurrenceRuleSection:
+            return 1;
+            
+        default:
+            return 0;
+    }
 }
 
-static NSString* recurrenceRuleCellID =         @"recurrenceRuleCell";
+static NSString* kSpecificRecurrenceRuleCellID =        @"specificRecurrenceRuleCell";
+static NSString* kCustomRecurrenceRuleCellID =          @"customRecurrenceRuleCell";
 
-static NSInteger customRecurrenceRuleSection =  1;
-static NSInteger recurrenceRuleTypeCount =      6;
+const static NSInteger kSpecificRecurrenceRuleSection = 0;
+const static NSInteger kCustomRecurrenceRuleSection =   1;
+
+const static NSInteger kNoneRecurrenceRuleRow =         0;
+const static NSInteger kDailyRecurrenceRuleRow =        1;
+const static NSInteger kWeekdaysRecurrenceRuleRow =     2;
+const static NSInteger kWeeklyRecurrenceRuleRow =       3;
+const static NSInteger kMonthlyRecurrenceRuleRow =      4;
+const static NSInteger kYearlyRecurrenceRuleRow =       5;
+
+static NSInteger kSpecificRuleTypeCount =               6;
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:recurrenceRuleCellID forIndexPath:indexPath];
+    switch (indexPath.section) {
+        case kSpecificRecurrenceRuleSection:
+            return [self specificRecurrenceRuleCellForTableView:tableView indexPath:indexPath];
+        case kCustomRecurrenceRuleSection:
+            return [self customRecurrenceRuleCellForTableView:tableView indexPath:indexPath];
+        default:
+            return nil;
+    }
+}
+
+- (UITableViewCell*)specificRecurrenceRuleCellForTableView:(UITableView*)tableView indexPath:(NSIndexPath*)indexPath
+{
+    UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:kSpecificRecurrenceRuleCellID];
     
-    //ECRecurrenceRule* cellRule = [self recurrenceRuleForIndexPath:indexPath];
-    cell.textLabel.text = @"test";//cellRule.localizedName;
+    ECRecurrenceRule* rule = [self recurrenceRuleForIndexPath:indexPath];
+    cell.textLabel.text = rule.localizedName;
+    
+    if (rule.type == self.recurrenceRule.type) {
+        cell.accessoryType = UITableViewCellAccessoryCheckmark;
+    } else {
+        cell.accessoryType = UITableViewCellAccessoryNone;
+    }
     
     return cell;
 }
 
+- (UITableViewCell*)customRecurrenceRuleCellForTableView:(UITableView*)tableView indexPath:(NSIndexPath*)indexPath
+{
+    UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:kCustomRecurrenceRuleCellID];
+    
+    ECRecurrenceRule* rule = [self recurrenceRuleForIndexPath:indexPath];
+    cell.textLabel.text = rule.localizedName;
+    
+    if (rule.type == self.recurrenceRule.type) {
+        cell.accessoryType = UITableViewCellAccessoryCheckmark;
+    } else {
+        cell.accessoryType = UITableViewCellAccessoryNone;
+    }
+    
+    return cell;
+}
 
+- (ECRecurrenceRule*)recurrenceRuleForIndexPath:(NSIndexPath*)indexPath
+{
+    switch (indexPath.section) {
+        case kSpecificRecurrenceRuleSection:
+            return [self specificRecurrenceRuleForIndexPath:indexPath];
+            
+        case kCustomRecurrenceRuleSection:
+            if (self.recurrenceRule.type == ECRecurrenceRuleTypeCustom) {
+                return self.recurrenceRule;
+            } else {
+                return [ECRecurrenceRule customRecurrenceRuleWithFrequency:EKRecurrenceFrequencyDaily interval:2];
+            }
+            
+        default:
+            return nil;
+    }
+}
+
+- (ECRecurrenceRule*)specificRecurrenceRuleForIndexPath:(NSIndexPath*)indexPath
+{
+    switch (indexPath.row) {
+        case kNoneRecurrenceRuleRow:
+            return [ECRecurrenceRule recurrenceRuleForRecurrenceType:ECRecurrenceRuleTypeNone];
+            
+        case kDailyRecurrenceRuleRow:
+            return [ECRecurrenceRule recurrenceRuleForRecurrenceType:ECRecurrenceRuleTypeDaily];
+            
+        case kWeekdaysRecurrenceRuleRow:
+            return [ECRecurrenceRule recurrenceRuleForRecurrenceType:ECRecurrenceRuleTypeWeekdays];
+            
+        case kWeeklyRecurrenceRuleRow:
+            return [ECRecurrenceRule recurrenceRuleForRecurrenceType:ECRecurrenceRuleTypeWeekly];
+            
+        case kMonthlyRecurrenceRuleRow:
+            return [ECRecurrenceRule recurrenceRuleForRecurrenceType:ECRecurrenceRuleTypeMonthly];
+            
+        case kYearlyRecurrenceRuleRow:
+            return [ECRecurrenceRule recurrenceRuleForRecurrenceType:ECRecurrenceRuleTypeYearly];
+            
+        default:
+            return nil;
+    }
+}
 
 @end
