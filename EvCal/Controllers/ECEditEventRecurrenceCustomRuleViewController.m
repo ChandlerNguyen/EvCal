52c20fad88088de4eb3480ddca76778c82c22a88
Thomas Leu <tomleu@gmail.com>
2015-07-09 14:04:36 +0800

    Connect picker and recurrence rule

--- EvCal/Controllers/ECEditEventRecurrenceCustomRuleViewController.m
+++ EvCal/Controllers/ECEditEventRecurrenceCustomRuleViewController.m
@@ -6,6 +6,7 @@
 //  Copyright (c) 2015 spitzgoby LLC. All rights reserved.
 //
 
+@import EventKit;
 #import "ECEditEventRecurrenceCustomRuleViewController.h"
 #import "ECRecurrenceRuleFormatter.h"
 
@@ -28,6 +29,9 @@
     
     self.recurrenceRulePicker.delegate = self;
     self.recurrenceRulePicker.dataSource = self;
+    
+    [self.recurrenceRulePicker selectRow:[self rowForInterval:self.recurrenceRule.rule.interval] inComponent:kRecurrencePickerIntervalComponent animated:NO];
+    [self.recurrenceRulePicker selectRow:[self rowForFrequency:self.recurrenceRule.rule.frequency] inComponent:kRecurrencePickerFrequencyComponent animated:NO];
 }
 
 - (NSArray*)frequencyNames
@@ -36,6 +40,7 @@
         NSMutableArray* mutableRuleNames = [[ECRecurrenceRuleFormatter defaultFormatter].ruleNames mutableCopy];
         [mutableRuleNames removeObject:[ECRecurrenceRuleFormatter defaultFormatter].noneRuleName];
         [mutableRuleNames removeObject:[ECRecurrenceRuleFormatter defaultFormatter].customRuleName];
+        [mutableRuleNames removeObject:[ECRecurrenceRuleFormatter defaultFormatter].weekdaysRuleName];
         
         _frequencyNames = [mutableRuleNames copy];
     }
@@ -47,6 +52,7 @@
 
 const static NSInteger kRecurrencePickerComponentsCount =       2;
 const static NSInteger kRecurrencePickerIntervalComponent =     0;
+const static NSInteger kRecurrencePickerFrequencyComponent =    1;
 
 - (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView
 {
@@ -65,9 +71,47 @@ const static NSInteger kRecurrencePickerIntervalComponent =     0;
 - (NSString*)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
 {
     if (component == kRecurrencePickerIntervalComponent) {
-        return [NSString stringWithFormat:@"%lu", (long)row];
+        return [NSString stringWithFormat:@"%lu", (long)[self intervalForPickerRow:row]];
     } else {
         return self.frequencyNames[row];
+    }
+}
+
+- (NSInteger)intervalForPickerRow:(NSInteger)row
+{
+    return row + 1;
+}
+
+- (EKRecurrenceFrequency)frequencyForPickerRow:(NSInteger)row
+{
+    return (EKRecurrenceFrequency)row;
+}
+
+- (NSInteger)rowForFrequency:(EKRecurrenceFrequency)frequency
+{
+    return (NSInteger)frequency;
+}
+
+- (NSInteger)rowForInterval:(NSInteger)interval
+{
+    return interval - 1;
+}
+
+- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component
+{
+    NSInteger intervalRow = [self.recurrenceRulePicker selectedRowInComponent:kRecurrencePickerIntervalComponent];
+    NSInteger frequencyRow = [self.recurrenceRulePicker selectedRowInComponent:kRecurrencePickerFrequencyComponent];
+    
+    self.recurrenceRule = [ECRecurrenceRule customRecurrenceRuleWithFrequency:[self frequencyForPickerRow:frequencyRow] interval:[self intervalForPickerRow:intervalRow]];
+    [self informDelegateRecurrenceRuleWasSelected];
+}
+
+#pragma mark - Delegate
+
+- (void)informDelegateRecurrenceRuleWasSelected
+{
+    if ([self.customRuleDelegate respondsToSelector:@selector(viewController:didSelectCustomRule:)]) {
+        [self.customRuleDelegate viewController:self didSelectCustomRule:self.recurrenceRule];
     }
 }
 
