a7b1b5f67e6ca8714f7b7d66c39859c978c4c96b
Thomas Leu <tomleu@gmail.com>
2015-07-10 18:04:25 +0800

    Add alarm tests to project

---
+++ EvCalTests/ECAlarmTests.m
@@ -0,0 +1,29 @@
+//
+//  ECAlarmTests.m
+//  EvCal
+//
+//  Created by Tom on 7/10/15.
+//  Copyright (c) 2015 spitzgoby LLC. All rights reserved.
+//
+
+// Frameworks
+#import <XCTest/XCTest.h>
+
+// EvCal Classes
+#import "ECAlarm.h"
+
+@interface ECAlarmTests : XCTestCase
+
+@end
+
+@implementation ECAlarmTests
+
+- (void)setUp {
+    [super setUp];
+}
+
+- (void)tearDown {
+    [super tearDown];
+}
+
+@end
