4f96dc06ad380c09c611a5c41fe11d159f0013dd
Thomas Leu <tomleu@gmail.com>
2015-08-15 18:22:51 -0500

    Add multipicker cell to project

---
+++ EvCal/Views/ECEditEventMultiPickerCell.h
@@ -0,0 +1,13 @@
+//
+//  ECEditEventMultiPickerCell.h
+//  EvCal
+//
+//  Created by Tom on 8/15/15.
+//  Copyright (c) 2015 spitzgoby LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ECEditEventMultiPickerCell : UITableViewCell
+
+@end
---
+++ EvCal/Views/ECEditEventMultiPickerCell.m
@@ -0,0 +1,21 @@
+//
+//  ECEditEventMultiPickerCell.m
+//  EvCal
+//
+//  Created by Tom on 8/15/15.
+//  Copyright (c) 2015 spitzgoby LLC. All rights reserved.
+//
+
+#import "ECEditEventMultiPickerCell.h"
+
+@implementation ECEditEventMultiPickerCell
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
