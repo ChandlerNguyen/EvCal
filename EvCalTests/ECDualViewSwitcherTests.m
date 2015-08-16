4133c914cfa3a834cabfbefe0196071bc4a1372f
Thomas Leu <tomleu@gmail.com>
2015-08-16 10:39:24 -0500

    Add test class for switcher view

---
+++ EvCalTests/ECDualViewSwitcherTests.m
@@ -0,0 +1,30 @@
+//
+//  ECDualViewSwitcherTests.m
+//  EvCal
+//
+//  Created by Tom on 8/16/15.
+//  Copyright (c) 2015 spitzgoby LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+#import "ECDualViewSwitcher.h"
+
+@interface ECDualViewSwitcherTests : XCTestCase
+
+@end
+
+@implementation ECDualViewSwitcherTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+
+@end
