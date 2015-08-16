d0a6c66ae98327dbd96f6a657bbac3c32bd45696
Thomas Leu <tomleu@gmail.com>
2015-08-16 10:18:28 -0500

    Stub out required methods

--- EvCal/Views/ECDualViewSwitcher.m
+++ EvCal/Views/ECDualViewSwitcher.m
@@ -10,12 +10,56 @@
 
 @implementation ECDualViewSwitcher
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+#pragma mark - Lifecycle and Properties
+
+- (instancetype)initWithFrame:(CGRect)frame primaryView:(nonnull UIView *)primaryView secondaryView:(nonnull UIView *)secondaryView
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        self.primaryView = primaryView;
+        self.secondaryView = secondaryView;
+        self.visibleView = primaryView;
+    }
+    
+    return self;
 }
-*/
+
+- (void)setPrimaryView:(UIView *)primaryView
+{
+    _primaryView = primaryView;
+    
+    [self addSubview:primaryView];
+}
+
+- (void)setSecondaryView:(UIView *)secondaryView
+{
+    _secondaryView = secondaryView;
+    
+    [self addSubview:secondaryView];
+}
+
+
+- (void)switchView:(BOOL)animated
+{
+    if (self.visibleView == self.primaryView) {
+        [self switchToSecondayView:animated];
+    } else {
+        [self switchToPrimaryView:animated];
+    }
+}
+
+- (void)switchToPrimaryView:(BOOL)animated
+{
+    if (self.visibleView != self.primaryView) {
+        DDLogDebug(@"Switching to primary view");
+    }
+}
+
+- (void)switchToSecondayView:(BOOL)animated
+{
+    if (self.visibleView != self.secondaryView) {
+        DDLogDebug(@"Switching to secondary view");
+    }
+}
 
 @end
