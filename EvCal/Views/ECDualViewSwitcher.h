f5568cd83fd1bd1ecc95875893d7da45283c902f
Thomas Leu <tomleu@gmail.com>
2015-08-16 10:17:03 -0500

    Add comments and custom init method

--- EvCal/Views/ECDualViewSwitcher.h
+++ EvCal/Views/ECDualViewSwitcher.h
@@ -10,12 +10,57 @@
 
 @interface ECDualViewSwitcher : UIView
 
-@property (nonatomic, weak) UIView* primaryView;
-@property (nonatomic, weak) UIView* secondaryView;
-@property (nonatomic, weak) UIView* visibleView;
+//------------------------------------------------------------------------------
+// @name Properties
+//------------------------------------------------------------------------------
 
-- (void)switchView;
-- (void)switchToPrimaryView;
-- (void)switchToSecondayView;
+@property (nonatomic, weak) UIView* __nullable primaryView;
+@property (nonatomic, weak) UIView* __nullable secondaryView;
+@property (nonatomic, weak) UIView* __nullable visibleView;
+
+//------------------------------------------------------------------------------
+// @name Initializing
+//------------------------------------------------------------------------------
+
+/**
+ *  Creates and returns a new view switcher with the given views. The primary 
+ *  view is set as the visible view.
+ *
+ *  @param frame         The frame for the view switcher.
+ *  @param primaryView   The primaryView for the view switcher, defaults to 
+ *                       visible view.
+ *  @param secondaryView The secondary view for the view switcher.
+ *
+ *  @return The newly created view switcher.
+ */
+- (nonnull instancetype)initWithFrame:(CGRect)frame primaryView:(nonnull UIView*)primaryView secondaryView:(nonnull UIView*)secondaryView;
+
+
+//------------------------------------------------------------------------------
+// @name Switching Views
+//------------------------------------------------------------------------------
+
+/**
+ *  Switches the currently visible view with the switchers other view.
+ *
+ *  @param animated Determines whether the view transition should be animated.
+ */
+- (void)switchView:(BOOL)animated;
+
+/**
+ *  Switches to the primary view. If the primary view is already visible this 
+ *  method has no effect.
+ *
+ *  @param animated Determines whether the view transition should be animated.
+ */
+- (void)switchToPrimaryView:(BOOL)animated;
+
+/**
+ *  Switches to the secondary view. If the primary view is already visible this
+ *  method has no effect.
+ *
+ *  @param animated Determines whether the view transition should be animated.
+ */
+- (void)switchToSecondayView:(BOOL)animated;
 
 @end
