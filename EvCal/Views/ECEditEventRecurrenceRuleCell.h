aac7078cfe3c62d3954394d5e9a52d6eed02de34
Thomas Leu <tomleu@gmail.com>
2015-08-17 14:54:42 -0500

    Add delegate protocol and documentation

--- EvCal/Views/ECEditEventRecurrenceRuleCell.h
+++ EvCal/Views/ECEditEventRecurrenceRuleCell.h
@@ -6,11 +6,34 @@
 //  Copyright (c) 2015 spitzgoby LLC. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
 @class ECRecurrenceRule;
+@class ECEditEventRecurrenceRuleCell;
+
+/**
+ *  The ECEditEventRecurrenceCellDelegate receives updates when the recurrence
+ *  cell updates its recurrence rule.
+ */
+
+@protocol ECEditEventRecurrenceRuleCellDelegate <NSObject>
+
+- (void)recurrenceCell:(ECEditEventRecurrenceRuleCell*)cell didSelectRecurrenceRule:(ECRecurrenceRule*)rule;
+
+@end
+
+#import <UIKit/UIKit.h>
 
 @interface ECEditEventRecurrenceRuleCell : UITableViewCell
 
+//------------------------------------------------------------------------------
+// @name Properties
+//------------------------------------------------------------------------------
+
+// The recurrence rule presented by the cell. Setting this value will cause the
+// cell to update its UI accordingly.
 @property (nonatomic, strong) ECRecurrenceRule* recurrenceRule;
+
+// The delegate to receive updates concerning changes to the recurrence rule
+// cell
+@property (nonatomic, weak) id<ECEditEventRecurrenceRuleCellDelegate> recurrenceRuleDelegate;
 
 @end
