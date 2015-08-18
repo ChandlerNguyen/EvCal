606a7ef4d8972e9139c44211750b3f6f4d043772
Thomas Leu <tomleu@gmail.com>
2015-08-18 12:48:44 -0500

    Add pickers to alarm cell

--- EvCal/Views/ECAlarmCell.m
+++ EvCal/Views/ECAlarmCell.m
@@ -7,17 +7,96 @@
 //
 
 #import "ECAlarmCell.h"
+#import "ECDualViewSwitcher.h"
+#import "ECAlarm.h"
+
+@interface ECAlarmCell() <UIPickerViewDataSource, UIPickerViewDelegate>
+
+@property (nonatomic, weak) IBOutlet UILabel* titleLabel;
+@property (nonatomic, weak) IBOutlet UILabel* infoLabel;
+@property (nonatomic, weak) IBOutlet ECDualViewSwitcher* pickerContainerView;
+@property (nonatomic, weak) UIPickerView* offsetAlarmPicker;
+@property (nonatomic, weak) UIDatePicker* absoluteDatePicker;
+
+@property (nonatomic, strong) NSArray* offsetAlarms;
+
+@end
 
 @implementation ECAlarmCell
 
-- (void)awakeFromNib {
-    // Initialization code
+#pragma mark - Lifecycle and Properties
+
+- (void)awakeFromNib
+{
+    [super awakeFromNib];
+    
+    [self setup];
 }
 
-- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
-    [super setSelected:selected animated:animated];
+- (void)setup
+{
+    [self setupPickers];
+}
 
-    // Configure the view for the selected state
+- (void)setupPickers
+{
+    UIPickerView* offsetAlarmPicker = [[UIPickerView alloc] init];
+    UIDatePicker* absoluteDatePicker = [[UIDatePicker alloc] init];
+    
+    offsetAlarmPicker.dataSource = self;
+    offsetAlarmPicker.delegate = self;
+    [absoluteDatePicker addTarget:self action:@selector(datePickerValueChanged:) forControlEvents:UIControlEventValueChanged];
+    
+    [self.pickerContainerView setPrimaryView:offsetAlarmPicker];
+    [self.pickerContainerView setSecondaryView:absoluteDatePicker];
+}
+
+- (NSArray*)offsetAlarms
+{
+    if (!_offsetAlarms) {
+        _offsetAlarms = [self createOffsetAlarms];
+    }
+    
+    return _offsetAlarms;
+}
+
+- (NSArray*)createOffsetAlarms
+{
+    return @[[ECAlarm alarmWithType:ECAlarmTypeNone],
+             [ECAlarm alarmWithType:ECAlarmTypeOffsetQuarterHour],
+             [ECAlarm alarmWithType:ECAlarmTypeOffsetHalfHour],
+             [ECAlarm alarmWithType:ECAlarmTypeOffsetOneHour],
+             [ECAlarm alarmWithType:ECAlarmTypeOffsetTwoHours],
+             [ECAlarm alarmWithType:ECAlarmTypeOffsetSixHours],
+             [ECAlarm alarmWithType:ECAlarmTypeOffsetOneDay],
+             [ECAlarm alarmWithType:ECAlarmTypeOffsetTwoDays]];
+}
+
+
+#pragma mark - UI Events
+
+- (void)datePickerValueChanged:(UIDatePicker*)sender
+{
+    DDLogDebug(@"Date picker value changed: %@", sender.date);
+}
+
+
+#pragma mark - UIPickerView delegate and data source
+
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView
+{
+    return 1;
+}
+
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
+{
+    return self.offsetAlarms.count;
+}
+
+- (NSString*)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
+{
+    ECAlarm* alarm = self.offsetAlarms[row];
+    return alarm.localizedName;
 }
 
 @end
