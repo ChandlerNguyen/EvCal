7a28f0d8dbb089a0cd0dba1664ac46cdd9c5f81d
Thomas Leu <tomleu@gmail.com>
2015-09-04 16:20:32 -0500

    Add month view to project

---
+++ EvCal/Views/ECMonthView.h
@@ -0,0 +1,13 @@
+//
+//  ECMonthView.h
+//  EvCal
+//
+//  Created by Tom on 9/4/15.
+//  Copyright (c) 2015 spitzgoby LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ECMonthView : UIView
+
+@end
---
+++ EvCal/Views/ECMonthView.m
@@ -0,0 +1,13 @@
+//
+//  ECMonthView.m
+//  EvCal
+//
+//  Created by Tom on 9/4/15.
+//  Copyright (c) 2015 spitzgoby LLC. All rights reserved.
+//
+
+#import "ECMonthView.h"
+
+@implementation ECMonthView
+
+@end
